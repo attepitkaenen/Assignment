43ab43b317198859258b677ff7ea9eeec481df8d
Atte Pitkänen <pitkasenatte@gmail.com>
2024-07-08 12:55:54 +0200

    Clean up before turning in Assignment

--- server/index.ts
+++ server/index.ts
@@ -3,28 +3,28 @@ import { restaurantRoutes } from "./services/restaurant.js";
 import { TypeBoxTypeProvider } from '@fastify/type-provider-typebox'
 import cors from '@fastify/cors'
 
-
 const app = fastify({
     logger: true
 }).withTypeProvider<TypeBoxTypeProvider>();
 
-app.register(restaurantRoutes, {prefix: '/api/restaurants'});
+
 app.register(cors, {
     origin: (origin, cb) => {
         if (typeof origin == 'string')
         {
             const hostname = new URL(origin).hostname
-
+            
             if(hostname === "localhost"){
-              //  Request from localhost will pass
-              cb(null, true)
-              return
+                //  Request from localhost will pass
+                cb(null, true)
+                return
             }
         }
         // Generate an error on other origins, disabling access
         cb(new Error("Not allowed"), false)
-      }
+    }
 })
+app.register(restaurantRoutes, {prefix: '/api/restaurants'});
 
 
 const main = async () => {
